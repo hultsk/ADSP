6cbeb52ce8f5d53fb98a70fd4e17e649e7f5c73e
Gustav Hultskär <gustav.hultskar@icore.se>
2023-10-13 22:19:17 +0200

    Add common FieldNames used in DevOps

---
+++ ADSP.ConsoleApplication/DevOps/FieldNames.cs
@@ -0,0 +1,27 @@
+﻿namespace ADSP.ConsoleApplication.DevOps;
+
+internal static class FieldNames
+{
+   public const string AreaPath = "System.AreaPath";
+   public const string BaselineWork = "Microsoft.VSTS.Scheduling.BaselineWork";
+   public const string CompletedWork = "Microsoft.VSTS.Scheduling.CompletedWork";
+   public const string RemainingWork = "Microsoft.VSTS.Scheduling.RemainingWork";
+   public const string ClosedBy = "Microsoft.VSTS.Common.ClosedBy";
+   public const string ResolvedBy = "Microsoft.VSTS.Common.ResolvedBy";
+   public const string StoryPoints = "Microsoft.VSTS.Scheduling.StoryPoints";
+   public const string Discipline = "Discipline";
+   public const string State = "System.State";
+   public const string AssignedTo = "System.AssignedTo";
+   public const string ChangedBy = "System.ChangedBy";
+   public const string ChangedDate = "System.ChangedDate";
+   public const string CreatedBy = "System.CreatedBy";
+   public const string CreatedDate = "System.CreatedDate";
+   public const string Description = "System.Description";
+   public const string IterationPath = "System.IterationPath";
+   public const string Reason = "System.Reason";
+   public const string Tags = "System.Tags";
+   public const string Title = "System.Title";
+   public const string WorkItemType = "System.WorkItemType";
+   public const string ReleaseNote = "iCore.VSTS.Common.ReleaseNote";
+   public const string ReleaseTo = "iCoreVSTS.Common.ServicePackTo";
+}
