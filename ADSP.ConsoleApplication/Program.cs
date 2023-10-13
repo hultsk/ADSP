938c594f20ccb5bca97268f04e6308f1006b988e
Gustav Hultskär <gustav.hultskar@icore.se>
2023-10-13 22:31:11 +0200

    Add template for ADSP.ConsoleApplication

--- ADSP.ConsoleApplication/Program.cs
+++ ADSP.ConsoleApplication/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ADSP.ConsoleApplication
+﻿using ADSP.ConsoleApplication.DevOps;
+using ADSP.ConsoleApplication.Utilities;
+using Microsoft.Extensions.Configuration;
+
+namespace ADSP.ConsoleApplication
 {
    internal class Program
    {
-      static void Main(string[] args)
+      private static async Task Main(string[] args)
       {
-         Console.WriteLine("Hello, World!");
+         IConfiguration configuration = InitializeConfiguration();
+
+         string pat = configuration["ApplicationSettings:PersonalAccessToken"] ?? throw new ArgumentNullException("PAT not provided in configuration");
+         string organizationUrl = configuration["ApplicationSettings:OrganizationUrl"] ?? throw new ArgumentNullException("Organization URL not provided in configuration");
+         string project = configuration["ApplicationSettings:Project"] ?? throw new ArgumentNullException("Project not provided in configuration");
+         string iteration = configuration["ApplicationSettings:Iteration"] ?? throw new ArgumentNullException("Iteration not provided in configuration");
+
+         var connection = AzureDevOpsConnectionManager.GetConnection(pat, organizationUrl);
+         var provider = new AzureDevOpsProvider(connection, project);
+
+         var workItems = await provider.GetWorkItemsForIterationAsync(iteration);
+
+         Console.WriteLine(workItems);
+
+        }
+
+      private static IConfiguration InitializeConfiguration()
+      {
+         var configBuilder = new ConfigurationBuilder()
+             .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+         return configBuilder.Build();
       }
    }
 }
